c66b0e3c3a46668e9852c486c38d342d70ec1e6a
Stefanie Vicente <stefanie.vicente@gmail.com>
2024-11-11 10:49:27 +0100

    refresh buildings data after delete

--- frontend/components/BuildingsListTable.tsx
+++ frontend/components/BuildingsListTable.tsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
 import {
   Paper,
   Table,
@@ -10,9 +11,38 @@ import {
   Typography,
 } from "@mui/material";
 import Row from "./BuildingsListTableRow";
+import { GET_BUILDINGS } from "../graphql/buildingQueries";
 import { IBuilding } from "@/types/IBuilding";
+import { useRouter } from "next/router";
 
-const BuildingsListTable = ({ buildings }: { buildings: IBuilding[] }) => {
+const BuildingsListTable = () => {
+  const router = useRouter();
+
+  const { data, loading, error, refetch } = useQuery(GET_BUILDINGS, {
+    fetchPolicy: "network-only",
+  });
+
+  const [currentBuildings, setCurrentBuildings] = useState<IBuilding[]>([]);
+
+  useEffect(() => {
+    if (data?.buildings) {
+      setCurrentBuildings(data.buildings);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    refetch();
+  }, [router, refetch]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const handleDeleteBuilding = (id: number) => {
+    setCurrentBuildings((prevBuildings) =>
+      prevBuildings.filter((building) => building.id !== id)
+    );
+  };
+
   return (
     <TableContainer component={Paper}>
       <Table aria-label="collapsible table">
@@ -32,8 +62,12 @@ const BuildingsListTable = ({ buildings }: { buildings: IBuilding[] }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {buildings?.map((building) => (
-            <Row key={building.id} building={building} />
+          {currentBuildings?.map((building: IBuilding) => (
+            <Row
+              key={building.id}
+              building={building}
+              onDelete={handleDeleteBuilding}
+            />
           ))}
         </TableBody>
       </Table>
