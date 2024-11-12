49228af9e29bcdc8e9f309abec0efc345def120a
Stefanie Vicente <stefanie.vicente@gmail.com>
2024-11-12 19:49:03 +0100

    fix frontend tests config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.test.json", // Point Jest to the test-specific tsconfig
+    },
+  },
+  testEnvironment: "jest-environment-jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Optional: for global setup
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest", // Transform TypeScript and JSX with ts-jest
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"], // Ensure Jest recognizes TypeScript and JSX
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1", // Support path aliases if needed
   },
